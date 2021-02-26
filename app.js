9ef072d35b5a867715234d7a04da6058821b7cc8
David Bassey <davidosky1@gmail.com>
2021-02-26 12:46:50 +0100

    Create skeleton for the html, css and js files

---
+++ app.js
@@ -0,0 +1,15 @@
+let myLibrary = [{
+  title: 'Harry Potter',
+  author: 'J.K.Rowling',
+  pages: 500,
+  readStatus: 'Already Read'
+}];
+
+function Book() {
+  // the constructor....
+} 
+
+
+function addBookToLibrary() {
+  // do stuff here
+}
