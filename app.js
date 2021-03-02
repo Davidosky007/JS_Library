fc9e10e218e97b93faaf5fc777fdfeea6ebc5e9f
David Bassey <davidosky1@gmail.com>
2021-03-02 15:16:27 +0100

    Add book constructor and class UI

--- app.js
+++ app.js
@@ -1,28 +1,50 @@
-let myLibrary = [{
-  title: 'Harry Potter',
-  author: 'J.K.Rowling',
-  pages: 500,
-  readStatus: 'Already Read'
-}];
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 
-function Book() {
-  // the constructor....
-} 
+// BOOK CLASS
+function Book(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+// UI CLASS
+// eslint-disable-next-line linebreak-style
+class UI {
+  static displayBooks() {
+    const books = Store.getBooks();
+
+    books.forEach((book) => UI.addBookToLibrary(book));
+  }
+
+
+  static addBookToLibrary(book) {
+    const list = document.getElementById('book-list');
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.pages}</td>
+  <td class="bookstatus" id='${book.title}${book.pages}'>${book.status}</td>
+  <td> <button class="btn btn-success changestatus" value='${book.status}'> change book status</button></td>
+  <td><a href="#" class="delete btn btn-danger">Delete</a></td>
+    `;
+    list.appendChild(row);
+  }
+}
 
 
 function addBookToLibrary() {
   // do stuff here
 }
 
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-  this.info = function() {
-     console.log(title, author)
-  }
-}
+// EVENT TO ADD CLASS TO DISPLAY FORM
 
-const author = new Book("The David's", "David Bassey", 120, "not read")
-author.info();
+function addClassToForm() {
+  document.getElementById('myForm').classList.toggle('form_list');
+}
+// Instantiate a book
+const book = new Book(title, author, pages, status);
