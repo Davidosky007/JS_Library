f3cffc7d07ef12fb39014ad5c031ad97a1cd0e99
GraceOyiza <graceoyiza13@gmail.com>
2021-03-02 13:48:47 +0100

    Create table list

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ function Book() {
 function addBookToLibrary() {
   // do stuff here
 }
+
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function() {
+     console.log(title, author)
+  }
+}
+
+const author = new Book("The David's", "David Bassey", 120, "not read")
+author.info();
