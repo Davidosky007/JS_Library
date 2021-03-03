bf31d84b5b378c53c5c9f35688be6c4d7af73edb
David Bassey <davidosky1@gmail.com>
2021-03-03 15:57:36 +0100

    Add library array

--- app.js
+++ app.js
@@ -1,50 +1,21 @@
-/* eslint-disable no-console */
-/* eslint-disable no-unused-vars */
-/* eslint-disable linebreak-style */
-
-// BOOK CLASS
-function Book(title, author, pages, status) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
-}
-
-// UI CLASS
-// eslint-disable-next-line linebreak-style
-class UI {
-  static displayBooks() {
-    const books = Store.getBooks();
-
-    books.forEach((book) => UI.addBookToLibrary(book));
-  }
-
-
-  static addBookToLibrary(book) {
-    const list = document.getElementById('book-list');
-    const row = document.createElement('tr');
-
-    row.innerHTML = `
-  <td>${book.title}</td>
-  <td>${book.author}</td>
-  <td>${book.pages}</td>
-  <td class="bookstatus" id='${book.title}${book.pages}'>${book.status}</td>
-  <td> <button class="btn btn-success changestatus" value='${book.status}'> change book status</button></td>
-  <td><a href="#" class="delete btn btn-danger">Delete</a></td>
-    `;
-    list.appendChild(row);
-  }
-}
-
-
-function addBookToLibrary() {
-  // do stuff here
-}
-
-// EVENT TO ADD CLASS TO DISPLAY FORM
-
-function addClassToForm() {
-  document.getElementById('myForm').classList.toggle('form_list');
-}
-// Instantiate a book
-const book = new Book(title, author, pages, status);
+let library;
+const DEFAULT_DATA = [
+  { name: 'The Lord of the Rings', author: 'Tolkien', pages: 266, status: 'read' },
+  {
+    name: 'Alice in Wonderland',
+    author: 'Lewis Caroll',
+    status: 'not read',
+  },
+  { name: 'Naruto', author: 'Masashi Kishimoto', pages: 543, status: 'read' },
+];
+const $name = document.querySelector('#name');
+const $author = document.querySelector('#author');
+const $status = document.querySelector('#status');
+const $pages = document.querySelector('#pages');
+const $tableBody = document.querySelector('#book-table-body');
+const $form = document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  render();
+  clearForm();
+});
